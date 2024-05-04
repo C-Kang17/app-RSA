3386837704d1c6fd9719883ac543c7998ce5bebe
TL17 <thanhlongm17@gmail.com>
2024-05-04 17:34:58 +0700

    ADD: content, encrypt, decrypt

---
+++ src/components/outputpublic.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import select from "./main";
+
+class Publickey extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="div-box-output">
+          <p>Your public key</p>
+          <div className="box-output">
+            <p className="text">
+              {this.props.data}
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default Publickey;
